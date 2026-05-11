c439462884142ba386f644e708f5db3050296329
Kiyozawaa <vegiitooo@gmail.com>
2026-05-11 18:55:10 +0530

    Add weapon level multiplier json

--- repo/weapons.repo.js
+++ repo/weapons.repo.js
@@ -7,12 +7,19 @@ export async function getWeaponList() {
 
 export async function getWeapon(id) {
   const details = await getWeaponDetails(id);
+  const passives = await getWeaponPassives(id);
   return {
-    ...details
+    ...details,
+    passives
   };
 }
 
 async function getWeaponDetails(id) {
   const rows = await db.get('SELECT * FROM weapons WHERE id = ?', id);
+  return rows;
+}
+
+async function getWeaponPassives(id) {
+  const rows = await db.all('SELECT * FROM weapon_passives WHERE id = ?', id);
   return rows;
 }
