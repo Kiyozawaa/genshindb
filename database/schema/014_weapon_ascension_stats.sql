0ea141c2d8ade6d1fdb2286ece781168c783d444
Kiyozawaa <vegiitooo@gmail.com>
2026-05-12 12:54:24 +0530

    Add weapon ascension stat table and fix base stat table

--- database/schema/014_weapon_ascension_stats.sql
+++ database/schema/014_weapon_ascension_stats.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS weapon_base_stats (
+CREATE TABLE weapon_ascension_stats (
   id INTEGER NOT NULL,
   stat TEXT NOT NULL,
-  value TEXT NOT NULL,
+  value REAL NOT NULL,
   growth TEXT NOT NULL,
-  PRIMARY KEY (id, stat, value),
+  PRIMARY KEY (id, stat, value, growth),
   FOREIGN KEY (id) REFERENCES weapons(id)
 );
