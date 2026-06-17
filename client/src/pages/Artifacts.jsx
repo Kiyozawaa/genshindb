e3c2f9b9efc2daa557b900aa93257f2ba923f634
Kiyozawaa <vegiitooo@gmail.com>
2026-06-17 14:05:49 +0530

    Update Artifact page cards

--- client/src/pages/Artifacts.jsx
+++ client/src/pages/Artifacts.jsx
@@ -16,29 +16,28 @@ export default function Artifacts() {
   }, []);
   
   if (!artifacts) return 'Loading...';
-  return (<>
-    <div className='content'>
-      <div className='item-list'>
-        {artifacts.map(artifact => (
+  
+  let result = artifacts?.sort((a, b) => b.levelList[1] - a.levelList[1]);
+
+  return (
+    <div className='item-list'>
+      {result.map(artifact => (
         <Item key={artifact.id} data={artifact}/>
-        ))}
-      </div>
+      ))}
     </div>
-    <NavBar/>
-  </>)
+  );
 }
 
 function Item({data}) {
   const iconUrl = `https://gi.yatta.moe/assets/UI/reliquary/`;
   return (
-    <Link className='a' to={`/artifact/${data.id}`}>
-    <div className='item-card'>
-      <img
-      className='icon'
-      src={iconUrl+data.icon+'.png'}/>
-      <div className='item-name'>
-      {data.name}</div>
+    <Link className='item-card__link' to={`/artifact/${data.id}`}>
+      <div className='item-card'>
+        <img className={`item-card__icon rarity-${data.levelList[1]}`} src={iconUrl+data.icon+'.png'}/>
+        <div className='item-card__name'>
+          {data.name}
+        </div>
       </div>
-      </Link>
+    </Link>
   );
 }
